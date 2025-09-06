3c65b1938c83b850df9e6c6f74c8d990d8a97a7b
nikitaarora18 <nikitaaror6918@gmail.com>
2025-09-06 22:49:34 +0530

    Add files via upload

---
+++ fourdigit.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int num, digit1, digit2, digit3, digit4, sum;
+    printf("Enter a four-digit integer: ");
+    scanf("%d", &num);
+    digit4 = num % 10;        
+    digit3 = (num / 10) % 10; 
+    digit2 = (num / 100) % 10; 
+    digit1 = (num / 1000) % 10; 
+    sum = digit1 + digit2 + digit3 + digit4;
+    printf("Sum of digits: %d\n", sum);
+
+    return 0;
+}
