c0e5ee317f3696725425eabb3354f6c52f6288f2
nikitaarora18 <nikitaaror6918@gmail.com>
2025-09-06 21:56:39 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int num1,num2;
+    printf("enter the first number:");
+    scanf("%d",&num1);
+    printf("enter the second number:");
+    scanf("%d",&num2);
+    int sum=num1+num2;
+    printf("the sum is %d",sum);
+    return 0;
+
+}
