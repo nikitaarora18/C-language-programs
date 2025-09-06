7e73e8828277d5520bfe6781792715f41bb23bba
nikitaarora18 <nikitaaror6918@gmail.com>
2025-09-06 22:33:13 +0530

    Add files via upload

---
+++ new.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    
+    printf("Nikita Arora\n");
+    printf("House No. 45, Green Valley\n");
+    printf("Sector 21\n");
+    printf("Gurgaon, Haryana - 122016\n");
+    printf("India\n");
+
+    return 0;
+}
